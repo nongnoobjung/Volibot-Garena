3a89205a5b6ad069cfd787eeac2ef457122e2263
nongnoobjung <kitcharuk_4@hotmail.com>
2015-01-07 18:09:54 +0700

    Fix Region PH
    Fix Region PH

--- PvPNETConnect/RTMP/Region.cs
+++ PvPNETConnect/RTMP/Region.cs
@@ -87,7 +87,7 @@ namespace LoLLauncher
         TH,
 
         [ServerValue("prodph.lol.garenanow.com")]
-        [LoginQueueValue("https://storeph.lol.garenanow.com/")]
+        [LoginQueueValue("https://lqph.lol.garenanow.com/")]
         [LocaleValue("en_US")]
         [UseGarenaValue(true)]
         PH,
